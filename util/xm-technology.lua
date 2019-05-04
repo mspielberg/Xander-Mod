40907484fb876bc36c5d36e8de9c5f08b7c49a9b
mspielberg <9729801+mspielberg@users.noreply.github.com>
2019-05-03 23:10:43 -0700

    simplify specification of science packs

--- util/xm-technology.lua
+++ util/xm-technology.lua
@@ -1,5 +1,14 @@
---Input parameters:	name	recipes to unlock	special effects		prerequisite technologies	unit count		science packs {{name, count}, {name, count}}	unit time cost		group number	subgroup number		individual technology number
+local packs = {
+  crude = {{"science-pack-0", 1}},
+  r     = {{"automation-science-pack", 1}},
+  rg    = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}},
+  rgb   = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}},
+  rgbp  = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}, {"production-science-pack", 1}},
+  rgby  = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}, {"utility-science-pack", 1}},
+  rgbpy = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}, {"production-science-pack", 1}, {"utility-science-pack", 1}},
+}
 
+--Input parameters:	name	recipes to unlock	special effects		prerequisite technologies	unit count		science packs {{name, count}, {name, count}}	unit time cost		group number	subgroup number		individual technology number
 function xm_technology(name_arg, recipe_unlocks, special_effects, prerequisites_arg, unit_count, unit_ingredients, unit_time, group_no, subgroup_no, tech_no)
 	--Create an output array for the prototype, using a few standard technology parameters
 	local out = {type = "technology", name = name_arg}
@@ -25,7 +34,7 @@ function xm_technology(name_arg, recipe_unlocks, special_effects, prerequisites_
 	--Set the prerequisites
 	out.prerequisites = prerequisites_arg
 	--Set the unit cost based on input parameters
-	out.unit = {count = unit_count, ingredients = unit_ingredients, time = unit_time}
+	out.unit = {count = unit_count, ingredients = packs[unit_ingredients], time = unit_time}
 	--Set the order based on input parameters
 	out.order = group_no .. "-" .. subgroup_no .. "-" .. tech_no
 	--Return the prototype array
